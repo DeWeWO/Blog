de18b35e3ceb455b2ae62ecbdec0982e7701c4d0
DeWeW <dewel000per@gmail.com>
2025-07-31 19:24:03 +0500

    blog

---
+++ auth/managers.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, first_name, last_name, discipline, user_group, password, **extra_fields):
+        if not username:
+            raise ValueError("Foydalanuvchi nomi kiritilishi kerak!")
+        if not first_name:
+            raise ValueError("Ism kiritilishi kerak!")
+        if not last_name:
+            raise ValueError("Familiya kiritilishi kerak!")
+        if not discipline:
+            raise ValueError("Yo'nalish kiritilishi kerak!")
+        if not user_group:
+            raise ValueError("Guruh kiritilishi kerak!")
+        
+        user = self.model(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            discipline=discipline,
+            user_group=user_group,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, username, first_name, last_name, discipline, user_group, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        
+        return self.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            discipline=discipline,
+            user_group=user_group,
+            password=password,
+            **extra_fields
+        )
--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 
 
+class User(AbstractUser):
+    discipline = models.CharField(max_length=255)
+    user_group = models.CharField(max_length=100)
+    
+    REQUIRED_FIELDS = ["first_name", "last_name", "discipline", "user_group"]
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return self.username
