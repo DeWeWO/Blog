8c661523139fd77420f50e23568df13a6b6cd5f6
DeWeW <dewel000per@gmail.com>
2025-07-31 16:07:45 +0500

    blog

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, blog_entries, post_details, contact
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('blog/', blog_entries, name='blog_entries'),
+    path('post-details/', post_details, name='post_details'),
+    path('contact/', contact, name='contact'),
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def about(request):
     return render(request, 'core/about.html')
+
+def blog_entries(request):
+    return render(request, 'core/blog.html')
+
+def post_details(request):
+    return render(request, 'core/post-details.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
