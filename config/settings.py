59afd7f798bcae23c7e277e0266f573011e029a7
DeWeW <dewel000per@gmail.com>
2025-07-31 17:08:41 +0500

    blog

--- config/settings.py
+++ config/settings.py
@@ -69,23 +69,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST', 'localhost'),
+        'PORT': env.str('DB_PORT', 5432)
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env.str('DB_NAME'),
-#         'USER': env.str('DB_USER'),
-#         'PASSWORD': env.str('DB_PASSWORD'),
-#         'HOST': env.str('DB_HOST', 'localhost'),
-#         'PORT': env.str('DB_PORT', 5432)
-#     }
-# }
-
 
 
 # Password validation
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 import requests
+from environs import Env
+
+env = Env()
+env.read_env()
 
 
 def index(request):
@@ -15,4 +19,19 @@ def post_details(request):
     return render(request, 'core/post-details.html')
 
 def contact(request):
+    BOT_TOKEN = env.str('BOT_TOKEN')
+    GROUP_CHAT_ID = env.str('GROUP_CHAT_ID')
+    URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        subject = request.POST.get("subject")
+        message = request.POST.get("message")
+        text = f"<b>Name: {name}\nEmail: {email}\nSubject: {subject}\nMessage: </b> <i>{message}</i>"
+        response = requests.post(url=URL, data={
+            "chat_id": GROUP_CHAT_ID,
+            "text": text,
+            "parse_mode": "HTML"
+        })
+        print(response)
     return render(request, 'core/contact.html')
