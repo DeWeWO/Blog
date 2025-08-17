767c1c38fa03b30cfba4ec9d3f56440445f76c4b
DeWeWO <dewel000per@gmail.com>
2025-08-18 00:48:26 +0500

    blog

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Post, PostImage
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug")
+    search_fields = ("title", "slug")
+    fields = ("id", "title", "slug", "description", "created", "updated")
+    prepopulated_fields = {"slug": ("title", )}
+    readonly_fields = ("id", "created", "updated")
+
+class PostImageAdmin(admin.StackedInline):
+    model = PostImage
+    extra = 2
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "category", "author")
+    prepopulated_fields = {"slug": ("title", )}
+    search_fields = ("title", "slug", "category")
+    fields = ("id", "title", "slug", "description", "category", "author", "created", "updated")
+    readonly_fields = ("id", "created", "updated")
+    list_filter = ("created", "updated", "category")
+    inlines = [PostImageAdmin]
