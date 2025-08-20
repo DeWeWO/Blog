4840589dcb80bebb836fa762db4c240c85bf4679
DeWeWO <dewel000per@gmail.com>
2025-08-21 00:38:25 +0500

    blog

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Post
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Field
+from django.forms import ModelChoiceField
 
 class MultipleFileInput(forms.ClearableFileInput):
     allow_multiple_selected = True
@@ -12,14 +15,59 @@ class MultipleFileField(forms.FileField):
     def clean(self, data, initial=None):
         single_file_clean = super().clean
         if isinstance(data, (list, tuple)):
-            result = [single_file_clean(d, initial) for d in data]
-        else:
-            result = single_file_clean(data, initial)
-        return result
+            return [single_file_clean(d, initial) for d in data]
+        return single_file_clean(data, initial)
 
 class PostForm(forms.ModelForm):
-    images = MultipleFileField(label='Select files', required=True)
-    
+    images = MultipleFileField(label='Post uchun rasm(lar)', required=True)
+
     class Meta:
         model = Post
         fields = ["title", "description", "category", "author"]
+        labels = {
+            "title": "Sarlavha",
+            "description": "Tavsif",
+            "category": "Kategoriya",
+            "author": "Muallif",
+        }
+        widgets = {
+            "title": forms.TextInput(attrs={
+                "placeholder": "Sarlavha kiriting"
+            }),
+            "description": forms.Textarea(attrs={
+                "placeholder": "To'liq tavsifni yozing",
+                "rows": 4
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if isinstance(self.fields.get("category"), ModelChoiceField):
+            self.fields["category"].empty_label = "--- Kategoriya tanlang ---"
+            
+        self.fields["title"].widget.attrs.update({
+            "title": "Sarlavha uchun joy"
+        })
+        
+        self.fields["description"].widget.attrs.update({
+            "title": "Postni to'liq yozing"
+        })
+        
+        self.fields["category"].widget.attrs.update({
+            "title": "Post qaysi sohaga tegishli"
+        })
+
+        self.fields["images"].widget.attrs.update({
+            "title": "Rasm(lar)ni yuklang"
+        })
+
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.layout = Layout(
+            Field("title"),
+            Field("description"),
+            Field("category"),
+            Field("author"),
+            Field("images")
+        )
